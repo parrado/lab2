78dc8d098860b4c5351578ed8c9de7418eb4db02
Alexander López-Parrado <parrado@uniquindio.edu.co>
2024-10-10 04:13:59 +0200

    Update users.py with description of functions returned values

--- users.py
+++ users.py
@@ -11,12 +11,16 @@ def registerUser(name,password):
 # Función que abre o cierra una sesión
 # Abre/cierra una sesión del usuario dependiendo del valor de flag 
 # lo hace si el nombre de usuario y la contraseña son correctos
+# Si la sesión se pudo abrir/cerrar debe retornar "Session was succesfully opened/closed" de lo contrario
+# debe retornar "error"
 def openCloseSession(name,password,flag):
     pass
 
 # Función que actualiza el puntaje
 # Actualiza el puntaje del usuario con el valor de score si el nombre de usuario y la contraseña 
 # son correctos y si el usuario se encuentra con sesión abierta
+# Si se pudo actualizar el puntaje debe retornar "Score was succesfully updated" de lo contrario
+# debe retornar "error"
 def updateScore(name,password,score):
     pass
 
@@ -24,18 +28,22 @@ def updateScore(name,password,score):
 # Función que lee el puntaje
 # Retorna el puntaje del usuario si el nombre de usuario y la contraseña 
 # son correctos y si el usuario se encuentra con sesión abierta
+# Si se pudo leer el puntaje debe retornar el puntaje de lo contrario
+# debe retornar "error"
 def getScore(name,password):
     pass
 
 # Función que lee la lista de usuarios conectados
 # retorna una lista con los usuarios conectados, solo debe devolver nombre y puntaje
 # si el nombre de usuario y la contraseña  son correctos y si el usuario se encuentra con sesión abierta
+# de lo contrario debe retornar "error"
 def usersList(name,password):
     pass
             
     
 # Función que genera una pregunta en una categoría cat
 # retorna la pregunta si el nombre de usuario y la contraseña  son correctos y si el usuario se encuentra con sesión abierta
+# de lo contrario debe retornar "error"
 def question(name,password,cat):
     pass
                 
